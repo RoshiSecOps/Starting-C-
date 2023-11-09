efd3f39a54a1b3b4c17d31045b108e3893deee36
ipopov <116061712+RoshiSecOps@users.noreply.github.com>
2023-11-09 13:22:27 +0200

    Learned how variables are declared, input, standard library, I/O and namespaces.

---
+++ coutex.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    cout << 34 << '\n';
+    cout << "hello" <<'\n';
+    //comment
+
+    cout<< '\n' << '\n';
+    int i = 2+2;
+    cout << i <<'\n';
+    i = i*3;
+    cout << i << '\n';
+
+    int j = 2;
+    cout << j << '\n';
+
+    i = 4.9;
+    j = 7/2;
+    cout << i << " " << j << '\n';
+
+    float f = 4.9;
+    cout << f << '\n';
+
+    f = 9.0/5;
+    cout << f << '\n';
+}
---
+++ input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main()
+{
+    int i;
+    cout << "Enter a number:";
+    cin >> i;
+    cout << i << '\n' << i*2 << '\n';
+}
